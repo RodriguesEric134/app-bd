c7d0ecfb19ccd97302a62f393e4c250d23f2d891
RodriguesEric134 <rm550249@fiap.com.br>
2025-03-26 15:38:38 -0300

    Add Usuario model and related components for user management

---
+++ src/app/data/constants/usuarios.ts
@@ -0,0 +1,22 @@
+import { Usuario } from "@prisma/client"
+
+const usuarios: Usuario[] =[
+    {
+        id: "1",
+        email: "ana@empresa.com.br",
+        nome: "Ana",
+        senha: "123456"
+    },
+    {
+        id: "2",
+        email: "victoria@empresa.com.br",
+        nome: "Victoria",
+        senha: "123456"
+    },
+    {
+        id: "3",
+        email: "lipe@empresa.com.br",
+        nome: "Lipe",
+        senha: "123456"
+    },
+]
